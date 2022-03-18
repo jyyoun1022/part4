40e611a3f2c80e34e94d2008ad0da67dbc61975f
jyyoun1022 <jyyoun1022@naver.com>
2022-03-18 22:51:59 +0900

    파일 업로드 처리

--- src/main/java/part4/part4/controller/UploadController.java
+++ src/main/java/part4/part4/controller/UploadController.java
@@ -2,39 +2,98 @@ package part4.part4.controller;
 
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import part4.part4.dto.UploadResultDTO;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Log4j2
 public class UploadController {
 
+    @Value("${part4.upload.path}")  //application.properties의 변수
+    private String uploadPath;
+
+
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) {
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
 
-////        for (MultipartFile uploadFile : uploadFiles) {
-////
-////            String originalName = uploadFile.getOriginalFilename();
-////            String fileName = originalName.substring(originalName.lastIndexOf("\\")+1);
-////
-////
-////            log.info("fileName: "+ fileName);
-//
-//
-//        }
-//    }
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+        for (MultipartFile uploadFile : uploadFiles) {
 
-        Arrays.stream(uploadFiles).forEach(i->{
-            String originalFilename = i.getOriginalFilename();
-            String fileName = originalFilename.substring(originalFilename.lastIndexOf("\\") + 1);
+            // 이미지 파일만 업로드 가능
+            if(uploadFile.getContentType().startsWith("image") == false){
+                // 이미지가 아닌경우 403 Forbidden 반환
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
 
-            log.info(fileName);
-        } );
+            // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
+            String originalName = uploadFile.getOriginalFilename();
 
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            //UUID
+            String uuid = UUID.randomUUID().toString();
+
+            //저장할 파일 이름
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);// 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
+
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+
+        private String makeFolder(){
+
+            String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+            String folderPath = str.replace("/", File.separator);
+
+            //make folder -------
+            File uploadPathFolder = new File(uploadPath,folderPath);
+
+            if(uploadPathFolder.exists()  == false) {
+                uploadPathFolder.mkdirs();
+            }
+            return folderPath;
+        }
+
+
+    /** 업로드 이미지 출력하기
+     */
+
     }
 
---
+++ src/main/java/part4/part4/dto/UploadResultDTO.java
@@ -0,0 +1,32 @@
+package part4.part4.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Data
+@AllArgsConstructor
+public class UploadResultDTO implements Serializable {
+    //implements Serializable을 하는 이유: 자바 시스템 내부에서 사용되는 Object 또는 Data를
+    // 외부의 자바 시스템에서도 사용할 수 있도록 바이트 형태로 데이터를 변환하는 기술로써,
+    //JVM 메모리에 상주되어 있는 객체 데이터를 바이트 형태로 변환하는 기술입니다.(직렬화)
+
+    private String fileName;
+
+    private String uuid;
+
+    private String folderPath;
+
+    public String getImageURL(){    //추후에 전체 경로가 필요한 경우를 대비하여 생성
+        try{
+            return URLEncoder.encode(folderPath+"/"+uuid+"_"+fileName,"UTF-8");
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
